99ab740e11f3602e48bbae405eafc59c8002f049
AyrielNoriega <anoriega2310@gmail.com>
2022-09-26 09:38:16 -0500

    refactory

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+//use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     public function upload(Request $request)
     {
+       // dd($request->file('photo'));
         $request->file('photo')->store('profiles');
 
         return redirect('profile');
